7d2d69c6aab13834b5bcd51d8cc56804bae0e9ec
지토지토 <iam@zito.kr>
2025-12-19 00:42:38 +0900

    add: @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MemberModule } from './domain/member/member.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MemberModule],
+  imports: [
+    MemberModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
