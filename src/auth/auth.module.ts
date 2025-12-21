4e0339b3b34f755fc61ed7b578c9880472c54e86
지토지토 <iam@zito.kr>
2025-12-21 23:01:05 +0900

    feat: Guard 전역 사용

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
@@ -11,7 +13,7 @@ import { jwtConstants } from './constants';
       signOptions: { expiresIn: '30m' }, //30분
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, { provide: APP_GUARD, useClass: AuthGuard }],
   exports: [AuthService],
 })
 export class AuthModule {}
