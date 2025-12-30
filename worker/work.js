3169b79ba7398e7241d490b409f8d049ea3263cc
지토지토 <iam@zito.kr>
2025-12-30 23:18:06 +0900

    img - webp 경로 설정

--- worker/work.js
+++ worker/work.js
@@ -5,7 +5,7 @@ const sharp = require("sharp");
 require("dotenv").config();
 
 //업로드 폴더에 그대로 넣음
-//const webpDir = path.join(process.cwd(), "webp");
+const webpDir = path.join(process.cwd(), "webp");
 console.log(process.cwd());
 
 if (!fs.existsSync(webpDir)) fs.mkdirSync(webpDir, { recursive: true });
@@ -20,12 +20,12 @@ const myWorker = new Worker(
     await Promise.all(
       files.map(async (fileName) => {
         //경로 확인
-        const inputPath = path.join("../backend/uploads", fileName);
+        const inputPath = path.join("uploads", fileName);
         if (!fs.existsSync(inputPath))
           return console.error("파일 없음:", inputPath);
 
         const outputFileName = path.parse(fileName).name + ".webp";
-        const outputPath = path.join(inputPath, outputFileName);
+        const outputPath = path.join(webpDir, outputFileName);
         sharp(inputPath).webp({ quality: 80 }).toFile(outputPath);
         console.log(`${fileName} -> ${outputFileName}`);
       })
