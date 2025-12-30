e88c8a7933ab98b54cd2e8096e757c66c6ab94de
지토지토 <iam@zito.kr>
2025-12-30 22:42:59 +0900

    feat: uploads 경로 수정

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -12,7 +12,7 @@ import { join } from 'path';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ServeStaticModule.forRoot({
-      rootPath: join(process.cwd(), '..', 'uploads'), // 업로드 폴더 경로
+      rootPath: join(process.cwd(), 'uploads'), // 업로드 폴더 경로
       serveRoot: '/uploads', // 브라우저에서 접근할
     }),
     MemberModule,
