81515eb5ab5a81f72af9fa65a3ce13d68da8adb7
지토지토 <iam@zito.kr>
2025-12-30 19:57:07 +0900

    npm i helmet

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './filter/globalExceptionFilter';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
   app.enableCors({
     origin: '*', // 전부 허용 (개발용)
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
